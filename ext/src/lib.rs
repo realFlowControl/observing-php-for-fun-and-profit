2cdc6c8acb27ef1f7fefb7cc9c6f649cd7818470
Florian Engelhardt <flo@dotbox.org>
2025-10-22 18:07:20 -0500

    add string helper

--- ext/src/lib.rs
+++ ext/src/lib.rs
@@ -1,4 +1,4 @@
-use ext_php_rs::prelude::*;
+use ext_php_rs::{ffi::_zend_string, prelude::*};
 
 #[php_function]
 pub fn hello_world(name: &str) -> String {
@@ -8,4 +8,17 @@ pub fn hello_world(name: &str) -> String {
 #[php_module]
 pub fn get_module(module: ModuleBuilder) -> ModuleBuilder {
     module.function(wrap_function!(hello_world))
+}
+
+#[allow(unused)]
+unsafe fn zend_string_to_string(zs: *const _zend_string) -> String {
+    if zs.is_null() {
+        return String::new();
+    }
+    unsafe {
+        let val_ptr = (*zs).val.as_ptr();
+        let len = (*zs).len;
+        let bytes = std::slice::from_raw_parts(val_ptr, len);
+        String::from_utf8_lossy(bytes).into_owned()
+    }
 }
